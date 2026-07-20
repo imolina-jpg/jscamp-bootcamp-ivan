d4c3d5a6f6abfdbb9f301c0070fa6b0938e6146e
madeval <alvarez.fing@gmail.com>
2026-07-19 22:23:08 -0300

    Add Route2 component to manage routing logic; implement path tracking and conditional rendering

--- 03-ejercicio-react/src/components/Route.jsx
+++ 03-ejercicio-react/src/components/Route.jsx
@@ -24,3 +24,25 @@ export function Route({ path, component }) {
 
   return <Component />
 }
+
+
+/* 1. Creamos el objeto fuera del componente, como tenemos una SPA, el objeto se creará una sola vez y no se destruirá aunque naveguemos entre páginas. */
+const availablePaths = new Set();
+
+// eslint-disable-next-line
+export function Route2({ path, component: Component }) {
+  const { currentPath } = useRouter();
+
+  /* 2. Cada vez que se llame el componente, lo guardamos en el objeto */
+  if (path) {
+    availablePaths.add(path);
+  }
+
+  /* 3. Si no hay path y la ruta actual no está registrada, cargamos el componente que le pasemos, en este caso el 404 porque se envía sin path */
+  if (!path && !availablePaths.has(currentPath)) return <Component />;
+
+  /* 4. Si la ruta actual no coincide con el path, no renderizamos nada */
+  if (currentPath !== path) return null;
+
+  return <Component />;
+}
